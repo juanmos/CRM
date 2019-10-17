ff2ee9e59c4f4d9fb7fe567fbe172d47fd609bc6
JuanMoscoso <juan.moscoso@primme.tech>
2019-10-16 19:08:07 -0500

    chequea tareas

---
+++ app/Console/Commands/TareasProximasCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\TareaProximaNotification;
+use Illuminate\Console\Command;
+use App\Models\Tarea;
+use Carbon\Carbon;
+
+class TareasProximasCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tareas:proximas';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tareas = Tarea::whereBetween('fecha',[Carbon::now()->toDateTimeString(),Carbon::now()->addMinutes(10)->toDateTimeString()])->where('realizado',0)->get();
+        foreach($tareas as $tarea){
+            $tarea->usuario->notify(new TareaProximaNotification($tarea->id));
+        }
+    }
+}
