a3d9def47a4eb5f7b6745630cd38e2e858a0a78c
JuanMoscoso <juan.moscoso@primme.tech>
2019-11-19 09:43:45 -0500

    cambio web

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -18,7 +18,9 @@
 						<p>CRM para gestión de equipos de trabajo<br/>
 						Descarga la app </p><p>
 							<a href="https://apps.apple.com/us/app/crm-soltecnologica/id1477497228?l=es&ls=1"><img src="{{asset('images/appstore.png')}}"/></a>  
-							<a href="https://html5up.net"><img src="{{asset('images/google.png')}}"/></a>.</p>
+							<a href="https://html5up.net"><img src="{{asset('images/google.png')}}"/></a>.
+						</p>
+						<a href="{{route('login')}}" class="button">Iniciar sesión</a>
 					</header>
 
 				<!-- Nav -->
